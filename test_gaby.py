6c5fc85ec98fac8d1f2d0d8f4850cc3543bc9098
gatheuws <gabriel.theuws@etu.uca.fr>
2020-12-10 11:51:29 +0100

    [AJOUT] utilisation argument pour mes tests

--- test_gaby.py
+++ test_gaby.py
@@ -2,22 +2,41 @@
 
 import numpy as np
 import pandas as pd
+import sys
+
+if len(sys.argv) < 2:
+	fichierAOuvir = 'Data/temp.dat'
+else :
+	fichierAOuvir = sys.argv[1]
+
+print('Ouverture du fichier : ',fichierAOuvir)
+
+try:
+	# Ouverture du fichier
+	f = open(fichierAOuvir, 'r')
+	# là ça fait une liste de liste
+	l = [ list(line.split('::')) for line in f ]
+except Exception as e:
+	print("le problème vient du fichier : ",fichierAOuvir)
+	raise e
+else:
+	print("Réussi")
+
 
 def print_list(liste):
-	print('la longueur de la liste est : ',len(liste),'\n','-'*8)
 	for i in liste:
 		print(i)
 
 
-f = open('Data/temp.dat', 'r')
-# là ça fait une liste de liste
-l = [ list(line.split('::')) for line in f ]
+def liste_to_dataFrame(liste):
+	for i in liste:
+		print_list(i)
 
+print('la longueur de la liste est : ',len(l),'\n','-'*8)
 print_list(l)
 
-#iterable = l.pop()
-#l.extend(iterable)
-#l.extend(iterable)
+liste_to_dataFrame(l)
 
-#list_iterator = l.pop() # on prend le dernier map object
-#print_iterator(list_iterator) # on l'itère
+#data = ["foo", "foofoo", "foofoofoo"]
+#index = ["1er element", "2eme element", "3eme element"]
+#s = pd.Series(data, index=index)
---
+++ test_gaby2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import sys
+
+if len(sys.argv) < 1:
+	print("usage ",sys.argv[0]," <nom du fichier>")
+	exit()
+fichierAOuvir = sys.argv[1]
+print('Ouverture du fichier : ',fichierAOuvir)
