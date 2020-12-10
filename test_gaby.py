98418dfe01ff5ec5abd7c0130ee2277be862f0bd
gatheuws <gabriel.theuws@etu.uca.fr>
2020-12-10 11:13:37 +0100

    [MODIF] map vers list pour une meilleure polyvalence

--- test_gaby.py
+++ test_gaby.py
@@ -1,20 +1,18 @@
 #python test
 
+import numpy as np
 import pandas as pd
 
-def function(file):
-	f = open(file, 'r')
-	fic = f.read()
-	f.close
-	return fic
-
 def print_iterator(it):
-    for x in it:
-        print(x)
+	print('la longueur de la liste est : ',len(it))
+	for x in it:
+		print(x)
 
 f = open('Data/temp.dat', 'r')
 # là ça fait une liste
-l = [ map(str,line.split('::')) for line in f ]
+l = [ list(line.split('::')) for line in f ]
 
-map_iterator = l.pop() # on prend le dernier map object
-print_iterator(map_iterator) # on l'itère
+print(l)
+print('-'*8)
+#list_iterator = l.pop() # on prend le dernier map object
+#print_iterator(list_iterator) # on l'itère
